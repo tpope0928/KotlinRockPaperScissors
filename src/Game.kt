200dfec2c31f4b7a774d90ad7d13e6a26aa64a76
tpope19 <40743479+tpope19@users.noreply.github.com>
2020-01-02 22:01:42 -0500

    game choice logic add

---
+++ src/Game.kt
@@ -0,0 +1,7 @@
+fun main(args: Array<String>) {
+    val options = arrayOf("Rock", "Paper", "Scissors")
+    val gameChoice = getGameChoice(options)
+}
+
+fun getGameChoice(optionsParam: Array<String>) =
+    optionsParam[(Math.random() * optionsParam.size).toInt()]
